a283c53209d9844f97cd78b3233bf7faf9a7b796
Dmitriy.Kravets <Dmitriy.Kravets@wolterskluwer.com>
2022-05-27 12:28:41 +0300

    ready to test

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/kdyepm/golang-united-school-homework-4.1
+
+go 1.18
--- reverse_string.go
+++ reverse_string.go
@@ -1,6 +1,10 @@
 package reverse_string
 
 func ReverseString(input string) (output string) {
-	// solution goes here
+	rns := []rune(input)
+	for i := 0; i < (len(rns))/2; i++ {
+		rns[i], rns[len(rns)-i-1] = rns[len(rns)-i-1], rns[i]
+	}
+	output = string(rns)
 	return output
 }
